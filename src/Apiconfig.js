73186061590347f600e5775daed13f1b4149526e
Dinesh Gokul <your_github_email@gmail.com>
2026-01-16 17:17:28 +0530

    Testing

--- src/Apiconfig.js
+++ src/Apiconfig.js
@@ -1,6 +1,6 @@
 const config = {
     // apiBaseUrl: 'https://erpdev.yjktechnologies.com:5547' 
-    apiBaseUrl: 'http://localhost:5547'
+    apiBaseUrl: 'http://192.168.29.243:5547'
   };
   
 module.exports = config;
